d13849d1920804068f7cb5cddd8622bdf1149cdc
ksdfg <ksdfg123@gmail.com>
2019-07-17 21:05:09 +0530

    shifted the main to bank, since it is more appropriate there

---
+++ src/AGSbank/Main.java
@@ -0,0 +1,61 @@
+package AGSbank;
+
+import AGSlibs.Client;
+import AGSlibs.Server;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        try (Bank bank = new Bank()){
+
+            String line;
+            JSONObject input = null;
+            Object result;
+
+            while (true){
+
+                try (Server server = new Server(5000)) {
+                    line = server.in.readUTF();
+                    input = (JSONObject) (new JSONParser()).parse(line);
+                }
+
+                String operation = (String) input.getOrDefault("operation", "");
+
+                switch (operation){
+                    case "authenticate":
+                        result = bank.authenticate((long)input.get("acc_no"), (int)input.get("pin"));
+                        break;
+
+                    case "getTransactions":
+                        result = bank.getTransactions((long)input.get("send_acc"), (long)input.get("recv_acc"));
+                        break;
+
+                    case "makeTransaction":
+                        bank.makeTransaction((long)input.get("send_acc"), (long)input.get("recv_acc"),
+                                123, (double)input.get("amount"));
+                        result = "transaction successful";
+
+                    default:
+                        System.out.println("bad operation");
+                        result = "";
+                }
+
+                JSONObject toWrite = new JSONObject();
+                toWrite.put("result", result);
+
+                try (Client client = new Client("", 5000)){
+                    client.out.writeBytes(toWrite.toJSONString());
+                }
+
+            }
+
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
--- src/AGSbroker/Main.java
+++ src/AGSbroker/Main.java
@@ -8,35 +8,6 @@ import org.json.simple.parser.JSONParser;
 public class Main {
 
     public static void main(String[] args){
-        try (Broker broker = new Broker()){
-
-            String line;
-            JSONObject input = null;
-
-            while (true){
-
-                try (Server server = new Server(5000)) {
-                    line = server.in.readUTF();
-                    input = (JSONObject) (new JSONParser()).parse(line);
-                }
-
-                String operation = (String) input.getOrDefault("operation", "");
-
-                switch (operation){
-                    default:
-                        System.out.println("bad operation");
-                }
-
-                try (Client client = new Client("", 5000)){
-                    client.out.writeUTF("");
-                }
-
-            }
-
-        }
-        catch (Exception e){
-            System.out.println(e.getMessage());
-        }
     }
 
 }
