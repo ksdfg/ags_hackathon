ea7230af41e7acdd09d1fc2068692a4e67b495b1
ksdfg <ksdfg123@gmail.com>
2019-09-07 20:09:42 +0530

    added dump file of database - will update as make changes

---
+++ ocps.sql
@@ -0,0 +1,231 @@
+-- MySQL dump 10.13  Distrib 8.0.16, for Win64 (x86_64)
+--
+-- Host: localhost    Database: ocps
+-- ------------------------------------------------------
+-- Server version	8.0.16
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8mb4 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `account`
+--
+
+DROP TABLE IF EXISTS `account`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `account` (
+  `ACC_NO` int(10) NOT NULL,
+  `BANK_ID` varchar(10) DEFAULT NULL,
+  `NAME` varchar(25) DEFAULT NULL,
+  `PIN` int(6) DEFAULT NULL,
+  `BALANCE` float(10,2) DEFAULT NULL,
+  PRIMARY KEY (`ACC_NO`),
+  KEY `BANK_ID` (`BANK_ID`),
+  CONSTRAINT `account_ibfk_1` FOREIGN KEY (`BANK_ID`) REFERENCES `bank` (`BANK_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `account`
+--
+
+LOCK TABLES `account` WRITE;
+/*!40000 ALTER TABLE `account` DISABLE KEYS */;
+/*!40000 ALTER TABLE `account` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `auth_keys`
+--
+
+DROP TABLE IF EXISTS `auth_keys`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `auth_keys` (
+  `USER_ID` varchar(25) NOT NULL,
+  `TYPE` varchar(15) NOT NULL,
+  `VALUE` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`USER_ID`,`TYPE`),
+  KEY `USER_ID` (`USER_ID`),
+  CONSTRAINT `auth_keys_ibfk_1` FOREIGN KEY (`USER_ID`) REFERENCES `user` (`USER_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `auth_keys`
+--
+
+LOCK TABLES `auth_keys` WRITE;
+/*!40000 ALTER TABLE `auth_keys` DISABLE KEYS */;
+/*!40000 ALTER TABLE `auth_keys` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `authorizes`
+--
+
+DROP TABLE IF EXISTS `authorizes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `authorizes` (
+  `USER_ID` varchar(25) NOT NULL,
+  `ACC_NO` int(10) NOT NULL,
+  PRIMARY KEY (`USER_ID`,`ACC_NO`),
+  KEY `USER_ID` (`USER_ID`),
+  KEY `ACC_NO` (`ACC_NO`),
+  CONSTRAINT `authorizes_ibfk_1` FOREIGN KEY (`USER_ID`) REFERENCES `user` (`USER_ID`),
+  CONSTRAINT `authorizes_ibfk_2` FOREIGN KEY (`ACC_NO`) REFERENCES `account` (`ACC_NO`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `authorizes`
+--
+
+LOCK TABLES `authorizes` WRITE;
+/*!40000 ALTER TABLE `authorizes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `authorizes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `bank`
+--
+
+DROP TABLE IF EXISTS `bank`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `bank` (
+  `BANK_ID` varchar(10) NOT NULL,
+  `NAME` varchar(30) DEFAULT NULL,
+  `CITY` varchar(11) DEFAULT NULL,
+  PRIMARY KEY (`BANK_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bank`
+--
+
+LOCK TABLES `bank` WRITE;
+/*!40000 ALTER TABLE `bank` DISABLE KEYS */;
+/*!40000 ALTER TABLE `bank` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `current`
+--
+
+DROP TABLE IF EXISTS `current`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `current` (
+  `SERVICE_CHG` float(10,2) DEFAULT NULL,
+  `ACC_NO` int(10) NOT NULL,
+  PRIMARY KEY (`ACC_NO`),
+  KEY `ACC_NO` (`ACC_NO`),
+  CONSTRAINT `current_ibfk_1` FOREIGN KEY (`ACC_NO`) REFERENCES `account` (`ACC_NO`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `current`
+--
+
+LOCK TABLES `current` WRITE;
+/*!40000 ALTER TABLE `current` DISABLE KEYS */;
+/*!40000 ALTER TABLE `current` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `savings`
+--
+
+DROP TABLE IF EXISTS `savings`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `savings` (
+  `INTEREST_RATE` float(10,2) DEFAULT NULL,
+  `ACC_NO` int(10) NOT NULL,
+  PRIMARY KEY (`ACC_NO`),
+  KEY `ACC_NO` (`ACC_NO`),
+  CONSTRAINT `savings_ibfk_1` FOREIGN KEY (`ACC_NO`) REFERENCES `account` (`ACC_NO`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `savings`
+--
+
+LOCK TABLES `savings` WRITE;
+/*!40000 ALTER TABLE `savings` DISABLE KEYS */;
+/*!40000 ALTER TABLE `savings` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `transactions`
+--
+
+DROP TABLE IF EXISTS `transactions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `transactions` (
+  `TRANS_ID` varchar(15) NOT NULL,
+  `TO_RECEIVER` varchar(20) DEFAULT NULL,
+  `FROM_SENDER` varchar(20) DEFAULT NULL,
+  `AMOUNT` float(10,2) DEFAULT NULL,
+  PRIMARY KEY (`TRANS_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transactions`
+--
+
+LOCK TABLES `transactions` WRITE;
+/*!40000 ALTER TABLE `transactions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `transactions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `user` (
+  `USER_ID` varchar(15) NOT NULL,
+  `NAME` varchar(15) DEFAULT NULL,
+  `PASSWORD` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`USER_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-09-07 20:08:59
