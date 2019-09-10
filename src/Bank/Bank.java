f782e95a46f921d00937596291603c78f8a423fb
ksdfg <ksdfg123@gmail.com>
2019-09-11 00:33:16 +0530

    fixed make transaction and get transactions

--- src/Bank/Bank.java
+++ src/Bank/Bank.java
@@ -7,10 +7,9 @@ import org.json.simple.JSONObject;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.SQLIntegrityConstraintViolationException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 import static java.lang.Math.abs;
 
@@ -24,7 +23,7 @@ public class Bank implements AutoCloseable {
     }
 
     // authenticate if credentials of user are correct
-    public boolean authenticate(long acc_no, int pin) throws Exception {
+    public boolean authenticate(int acc_no, int pin) throws Exception {
         // get result of pin
         ResultSet resultSet = da.getData(
                 "account",
@@ -40,39 +39,48 @@ public class Bank implements AutoCloseable {
         return acc_pin == pin;  // check if pin is correct
     }
 
-    public Map getTransactions(long acc) throws Exception {
+    public ArrayList getTransactions(int acc) throws Exception {
         // get trans_table
         ResultSet resultSet = da.getData(
                 "transactions t, account s, account r",
                 "t.sender = s.acc_no and t.receiver = r.acc_no",
-                "t.sender as sno, s.name as sname, t.receiver as rno, r.name as rname, t.amount as amount",
-                "t.sender = " + acc + "or t.receiver = " + acc
+                "t.sender as sno, s.name as sname, t.receiver as rno, r.name as rname, t.amount as amount," +
+                        " t.timestamp as timestamp",
+                "t.sender = " + acc + " or t.receiver = " + acc + " order by t.timestamp"
         );
 
-        Map<String, Double> m = new HashMap<>();    // map to store all transactions
-        while(resultSet.next()){
+        ArrayList<Vector> a = new ArrayList<>();    // map to store all transactions
+        Vector v;   // temp to add a transaction (other_party, amount) to arraylist
+        while (resultSet.next()) {
+            v = new Vector();
+
             // if acc is sender, then he's lost money
-            if (resultSet.getInt("sno") == acc)
-                m.put(resultSet.getString("rname"), resultSet.getDouble("amount") * -1);
+            if (resultSet.getInt("sno") == acc) {
+                v.add(resultSet.getString("rname"));
+                v.add(resultSet.getDouble("amount") * -1);
+            }
             // if acc isn't sender, he's gained money
-            else
-                m.put(resultSet.getString("sname"), resultSet.getDouble("amount"));
+            else {
+                v.add(resultSet.getString("sname"));
+                v.add(resultSet.getDouble("amount"));
+            }
+
+            a.add(v);   // add transaction to arraylist
         }
 
-        return m;   // kaeritai
+        return a;   // kaeritai
     }
 
     // add a transaction to the user
-    public void makeTransaction(long send_acc, long recv_acc, double amount) throws Exception {
+    public void makeTransaction(int send_acc, int recv_acc, double amount) throws Exception {
         // get current balance and trans_table of acc_no - send
         ResultSet sender = da.getData(
-                "account", "balance, name", "acc_no = " + send_acc
+                "account", "balance", "acc_no = " + send_acc
         );
         // check if transaction is valid
         if (!sender.next()) {      // if no account
             throw new Exception("Account number not in bank\n");
-        }
-        else if (sender.getDouble("balance") < amount) {  // banda bohot gareeb
+        } else if (sender.getDouble("balance") < amount) {  // banda bohot gareeb
             throw new Exception("Account number " + send_acc + " has insufficient funds\n");
         }
 
@@ -80,7 +88,7 @@ public class Bank implements AutoCloseable {
 
         // get current balance and trans_table of acc_no - recv
         ResultSet recipient = da.getData(
-                "customers", "trans_table, balance, name", "acc_no =" + recv_acc
+                "account", "balance", "acc_no =" + recv_acc
         );
         // check if transaction is valid
         if (!recipient.next()) {      // if no account
@@ -88,11 +96,10 @@ public class Bank implements AutoCloseable {
         }
 
         double recv_balance = recipient.getDouble("balance");
-        String recv_name = recipient.getString("name");
 
         // update balance - send
         da.update(
-                "customers",
+                "account",
                 "acc_no = " + send_acc,
                 "balance",
                 "" + (send_balance - amount)
@@ -100,7 +107,7 @@ public class Bank implements AutoCloseable {
 
         // update balance - recv
         da.update(
-                "customers",
+                "account",
                 "acc_no = " + recv_acc,
                 "balance",
                 "" + (recv_balance + amount)
@@ -111,11 +118,11 @@ public class Bank implements AutoCloseable {
             try {
                 da.addRow(
                         "transactions",
-                        abs((new Random()).nextLong()) + ", " + recv_acc + ", " + send_acc + ", " + (amount)
+                        abs((new Random()).nextLong()) + ", " + send_acc + ", " + recv_acc + ", " + (amount) +
+                                ", '" + DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(LocalDateTime.now()) + "'"
                 );
                 break;
-            }
-            catch (SQLIntegrityConstraintViolationException e){
+            } catch (SQLIntegrityConstraintViolationException e) {
                 System.out.println("duplicate trans id");
             }
         }
@@ -135,8 +142,12 @@ class TestBank {
 
         try (Bank bank = new Bank()) {
             System.out.println("it works...");
+            bank.makeTransaction(1, 2, 0.5);
+            System.out.println(bank.getTransactions(1));
+            ;
         } catch (Exception e) {
             System.out.println(e.getMessage());
+            e.printStackTrace();
         }
 
     }
